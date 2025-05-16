b910d30ed301e976a6d74e754799c32d44370a70
Advay254 <churchillmwegi@gmail.com>
2025-05-16 16:02:16 +0300

    Create db.py

---
+++ utils/db.py
@@ -0,0 +1,6 @@
+# Implement the database-related functions to save and retrieve content information
+   def save_content(content_type: str, content_data: dict):
+       pass
+
+   def get_content(content_type: str, **filters):
+       pass
